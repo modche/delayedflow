ad032b6a291eb2b95607c2b9d1020ea7a6ab74ac
Michael Stölzle <michaelstoelzle@me.com>
2020-09-24 11:40:14 +0200

    further updates and documentation

--- R/dfi_n.R
+++ R/dfi_n.R
@@ -1,28 +1,58 @@
-#' Calculation of DFI for block length of N
+#' Calculation of DFI for block length of n
 #'
-#' @param q streamflow series
-#' @param n block length
-#' @param ... other parameters passed to function
-#'
-#' @return
+#' @description Common baseflow separation separates quick- and baseflow with a
+#'   block length of n = 5 with \code{baseflow(q_obs, n = 5)}. To generate multiple separations
+#'   for the DFI index \code{n} vary between 1 and \code{n_max} (e.g. 60, 90 or 120 days). To do this the
+#'   function could be used with a \code{n} as a vector instead of a single integer (e.g. \code{baseflow(q_obs, n = 1:60)})
+#'   Note, separation with \code{n=0} is not possible. The DFI of \code{n=1} is not 1 as due to the algorithm some
+#'   streamflow peaks are separated from delayed flow.
+#' @param q vector with streamflow series
+#' @param n block length \code{n}. If \code{n} is a integer (vector of length one) a single
+#'   delayed flow separation is performed. If \code{n} is a vector (with length > 1)
+#'   multiple separations for all \code{n} values are calculated. To analyze later on the
+#'   DFI curve it is recommended that \code{n} starts at 1 and is a continuous vector (e.g. 1,2,3,.... 58, 59, 60).
+#'   But also variations like \code{n = c(1,5,10,15,30)} are possible.
+#' @param add_zero logical, default is \code{TRUE}. If \code{TRUE}, \code{n=0} and \code{dfi=1} is added to
+#'     output data.frame. Note that the \code{DFI} of a separation with \code{n=0} is by definition \code{DFI = 1}.
+
+#' @return A data.frame with two columns: \code{n} for block length and \code{dfi} with the \code{DFI(n)} values.
+#'     If \code{n} is a single integer the function returns a single integer (i.e. one \code{DFI(n)} value).
 #' @export
 #'
 #' @examples
-#' dfi_n(q_obs, n = 2:5)
-dfi_n <- function(q, n = 5) {
+#' # same as BFI calculation
+#' dfi_n(q_obs, n = 5)
+#'
+#' # 10 different separations based on various block length n
+#' dfi_n(q_obs, n = 1:10)
+#'
+#' # only specific block lengths are used
+#' dfi_n(q_obs, n = c(1,5,10,30,60,90), add_zero = FALSE)
+#'
+dfi_n <- function(q, n = 5, add_zero = TRUE) {
+
+	if (0 %in% n) stop("DFI calculation not possible if n = 0. Set add_zero = TRUE.")
 
 	if(length(n) == 1){
 		bflow <- baseflow(q, block.len = n)
 		valid <- !is.na(q) & !is.na(bflow)
 		return(sum(bflow[valid]) / sum(q[valid]))
 	} else {
-		if(min(n) != 1) stop('If n is given as vector the minimum of n should be 1.')
-		df <- data.frame(n = 0, dfi = 1)
+		if(min(n) != 1) warning('If n is given as vector the minimum of n should be 1.')
+
+		if(add_zero){
+			df <- data.frame(n = 0, dfi = 1)
+		}
 		for (i in 1:length(n)) {
 			bflow <- baseflow(q, block.len = n[i])
 			valid <- !is.na(q) & !is.na(bflow)
 			dfi <- (sum(bflow[valid]) / sum(q[valid]))
-			df <- rbind(df, c(n[i], dfi))
+			if(!add_zero & i == 1) {
+				df <- data.frame(n = n[i], dfi = dfi)
+			} else {
+				df <- rbind(df, c(n[i], dfi))
+			}
+
 		}
 		return(df)
 	}
--- R/find_bps.R
+++ R/find_bps.R
@@ -2,32 +2,45 @@
 #'
 #' @description Estimates n breakpoints in the DFI curve.
 #'     DFI values are compared against n+1 piecewise linear segments and the minimization of the
-#'     corresponding residuals. Several parameters can be tweaked.
+#'     corresponding residuals. Several parameters can be customized.
+#'     If \code{q_obs} is given in daily temporal resolution then also \code{n} can be interpreted as delay in days
+#'     for different contributions (default).
 #'
 #' @param dfi numeric, a vector with the DFI values between 1 and 0, dummy data [dfi_example] can be used.
 #' @param n_bp numeric, How many breakpoints (1, 2, 3) should be estimated? Default = 2
 #' @param min_gp_gap numeric, smallest interval between two breakpoints. Default = 5 (days)
 #' @param filter_min numeric, `filter_min`+1 is minimum potential breakpoint estimate
 #' @param filter_max numeric, `filter_max`-1 is maximum potential breakpoint estimate
+#' @param of_weights vector with two elements, first is weight of the RMSE, second is weight of the MAE.
+#'     Default is c(0.5, 0.5), i.e. equal weights. Sum of vector must be 1. To switch of one measure use
+#'     \code{c(1, 0)} or \code{c(0,1)}. With the weighting more or less focus could be given to the upper parts
+#'     of the DFI curve (i.e. when block length \code{n} for separation is between 1 and 10).
 #' @param dfi_check logical, if `TRUE` DFI values are converted to be monotonically descreasing with [cummin()]
 #' @param print logical, if `TRUE` best breakpoint estimates during calculation are printed (debug mode)
 #' @param plotting logical, if `TRUE` the DFI curve and piecewise linear segments are plotted with \code{plot()}
 #'
 #' @return Returns a list.
 #' \item{breakpoints}{estimates for the n breakpoints with names `bp_x`}
-#' \item{bias}{value of the objective function `OF = 1/2 RMSE + 1/2 MAE`}
+#' \item{bias}{value of the objective function
+#'     `OF = 1/2 RMSE + 1/2 MAE`,
+#'     where OF is to be minimized.}
 #' \item{rel_contr}{Relative streamflow contributions between \code{filter_min}, the breakpoints and \code{filter_max},
-#' e.g. 2 breakpoints lead to 3 relative contributions, 3 breakpoints lead to 4 relative contributions}
+#'     e.g. 2 breakpoints lead to 3 relative contributions, 3 breakpoints lead to 4 relative contributions.
+#'     The first \code{rel_contr}-avlue is the fastest contribution to streamflow, the last \code{rel_contr}-value
+#'     is the slowest contribution to streamflow.}
 #' @references Stoelzle, M., Schuetz, T., Weiler, M., Stahl, K., & Tallaksen, L. M. (2020). Beyond binary baseflow separation: a delayed-flow index for multiple streamflow contributions. Hydrology and Earth System Sciences, 24(2), 849-867.
 #' @export
 #' @examples
 #' # use dfi_example as an DFI vector with 121 values
 #' find_bp(dfi_example, n_bp = 2, filter_max = 90, dfi_check = FALSE)
+#'
+#' find_bp(runif(100), plotting = TRUE) # dummy code
 find_bp <- function(dfi,
 					n_bp = 2,
 					min_gp_gap = 5,
 					filter_min = 0,
 					filter_max = length(dfi) - 1,
+					of_weights = c(0.5, 0.5),
 					dfi_check = TRUE,
 					print = FALSE,
 					plotting = FALSE) {
@@ -49,6 +62,11 @@ find_bp <- function(dfi,
 	if(!n_bp %in% 1:3) {
 		stop("Number of breakpoints must be 1,2 or 3")
 	}
+
+	if(sum(of_weights) != 1) {
+		stop("Both weights must sum up to 1, please adjust of_weights.")
+	}
+
 	if(dfi_check) {
 		dfi <- cummin(dfi)
 	}
@@ -77,12 +95,12 @@ find_bp <- function(dfi,
 	# fit breakpoint model
 	dummy <- 999
 	best_bps <- NULL
-	cat("Calculating breakpoints . . . \n")
+	cat("Calculating breakpoints . . . \n 0%  ")
 	for (i in 1:nrow(bp_grid)) {
 
 		bps <- bp_grid[i,]
 		fit <- approx(x = c(0,bps,len-1), dfi[c(1,bps+1,len)], xout = 0:(len-1))$y
-        OF <-  rmse(fit,dfi) * 0.50 + mae(fit, dfi) * 0.50
+        OF <-  rmse(fit,dfi) * of_weights[1] + mae(fit, dfi) * of_weights[2]
     	if(OF < dummy){
 			if(print) cat(i,bps,OF,"\n")
 			dummy <- OF
@@ -90,7 +108,7 @@ find_bp <- function(dfi,
 			result <- list(breakpoints = bps, bias = dummy )
 		}
 
-        if(i %in% floor(nrow(bp_grid) * 1:10/10)) cat(paste0(round(i/nrow(bp_grid)*100,0)),"%  ")
+        if(i %in% floor(nrow(bp_grid) * 1:4/4)) cat(paste0(round(i/nrow(bp_grid)*100,0)),"%  ")
 
 	}
 	cat("\nBreakpoints ready . . . \n\n\n")
